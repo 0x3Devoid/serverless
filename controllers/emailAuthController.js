9969dd9b4c4c8353faf5f29bd92e3984aa612015
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-11-19 10:19:31 +0100

    mailing service

--- controllers/emailAuthController.js
+++ controllers/emailAuthController.js
@@ -2,78 +2,126 @@ const dbClient = require("../storage/db");
 const generateJWT = require("../utils/generateJWT");
 const bcrypt = require("bcryptjs");
 const dotenv = require("dotenv");
+const sendCode = require("../utils/welcomeMessage");
+const crypto = require("crypto");
+require("dotenv").config();
 
 dotenv.config();
 const salt = bcrypt.genSaltSync(10);
 class userAuthentication {
   static async login(req, res) {
     const { email, password } = req.body;
+
     if (!email) {
-      res.status(400).json({ error: "email not found" });
+      return res.status(400).json({ error: "Email not found" });
     }
+
     if (!password) {
-      res.status(400).json({ error: "password not found" });
+      return res.status(400).json({ error: "Password not found" });
     }
 
     if (typeof email !== "string" || typeof password !== "string") {
-      return res.status(400).json({ error: "invalid username/password" });
+      return res.status(400).json({ error: "Invalid email/password format" });
     }
-    const collection = await dbClient.db.collection('Users');
+
+    const collection = await dbClient.db.collection("Users");
+    const TokenCollection = await dbClient.db.collection("token");
 
     try {
-        const user = await collection.findOne({ "local.email": email });
-        if (!user) {
-          return res.status(404).json({ error: "User does not exist" });
-        }
-        const validPwd = await bcrypt.compareSync(password, user.local.password);
-        if (!validPwd) {
-          return res.status(401).json({ error: "Invalid password" });
-        }
-        const refreshToken = generateJWT({ email: user.email, username: user.username, id: user._id, duration: "1d" });
-        user.lastLogin = new Date();
-        user.refreshToken = refreshToken;
+      const user = await collection.findOne({ "local.email": email });
 
-        await collection.updateOne(
-          { "local.email": email },
-          {
-            $set: {
-              refreshToken: refreshToken,
-              lastLogin: new Date()
-            }
-          }
-        );  
-        return res.status(200).json({ message: "User logged in successfully" });
-      } catch (error) {
-        console.log(error);
-        return res.status(500).json({ error });
+      if (!user) {
+        return res.status(404).json({ error: "User does not exist" });
       }
-      
+
+      const validPwd = await bcrypt.compareSync(password, user.local.password);
+
+      if (!validPwd) {
+        return res.status(401).json({ error: "Invalid password" });
+      }
+
+      if (!user.local.verified) {
+        let token = await TokenCollection.findOne({
+          username: user.local.username,
+        });
+
+        if (!token) {
+          token = {
+            username: user.local.username,
+            token: crypto.randomBytes(32).toString("hex"),
+            createdAt: new Date(),
+          };
+
+          const url = `${process.env.BASE_URL}/users/${user.local.username}/verify/${token.token}`;
+          await sendCode(email, url);
+          await TokenCollection.insertOne(token);
+          const message =
+            "An email has been sent to your account, please verify";
+          return res.status(200).json({ message });
+        } else {
+          await TokenCollection.deleteOne({ username: user.local.username });
+
+          token = {
+            username: user.local.username,
+            token: crypto.randomBytes(32).toString("hex"),
+            createdAt: new Date(),
+          };
+          const url = `${process.env.BASE_URL}/users/${user.local.username}/verify/${token.token}`;
+          await sendCode(email, url);
+          await TokenCollection.insertOne(token);
+          const message =
+            "A new email has been sent to your account, please verify";
+          return res.status(200).json({ message });
+        }
+      }
+
+      const refreshToken = generateJWT({
+        email: user.email,
+        username: user.username,
+        id: user._id,
+        duration: "1d",
+      });
+      user.lastLogin = new Date();
+      user.refreshToken = refreshToken;
+
+      await collection.updateOne(
+        { "local.email": email },
+        {
+          $set: {
+            refreshToken: refreshToken,
+            lastLogin: new Date(),
+          },
+        }
+      );
+
+      return res.status(200).json({ message: "User logged in successfully" });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
   }
 
   static async logout(req, res) {
     const email = req.params.email;
 
-
     if (!email) {
-        return res.status(401).json({ error: "User not authenticated" });
+      return res.status(401).json({ error: "User not authenticated" });
     }
 
-    const collection = await dbClient.db.collection('Users');
+    const collection = await dbClient.db.collection("Users");
 
     try {
-        await collection.updateOne(
-            { "local.email": email },
-            { $unset: { refreshToken: 1 } }
-        );
+      await collection.updateOne(
+        { "local.email": email },
+        { $unset: { refreshToken: 1 } }
+      );
 
-        return res.status(200).json({ message: "User logged out successfully" });
+      return res.status(200).json({ message: "User logged out successfully" });
     } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: "Internal server error" });
+      console.error(error);
+      return res.status(500).json({ error: "Internal server error" });
     }
-}
-
-  
+  }
 
   //DONE
   static async register(req, res) {
@@ -95,23 +143,23 @@ class userAuthentication {
       return res.status(400).json({ error: "invalid username/password/email" });
     }
     const collection = await dbClient.db.collection("Users");
+    const TokenCollection = await dbClient.db.collection("token");
+
     try {
       const duplicate = await collection.findOne({ "local.email": email });
-      const dup_username = await collection.findOne({ "local.username": username });
-      const validRef = await collection.findOne({"local.username": referal});
-      
- 
+      const dup_username = await collection.findOne({
+        "local.username": username,
+      });
+      const validRef = await collection.findOne({ "local.username": referal });
+
       if (duplicate) {
         return res.status(409).json({ error: "Account Already Exist" });
       }
-      if(dup_username){
+      if (dup_username) {
         return res.status(409).json({ error: "Username Already Exist" });
-
       }
-     
-
       const password_hash = await bcrypt.hashSync(password, salt);
-      const newUser ={
+      let newUser = {
         method: "local",
         local: {
           username: username,
@@ -120,16 +168,57 @@ class userAuthentication {
           lastLogin: new Date(),
           earning: earning || 0,
           referal: validRef ? referal : null,
-          claimedRef: false
+          claimedRef: false,
+          verified: false,
+          code: 0,
         },
       };
       await collection.insertOne(newUser);
-      return res.status(201).json({ message: "User registered successfully" });
+      const token = {
+        username: newUser.local.username,
+        token: crypto.randomBytes(32).toString("hex"),
+        createdAt: new Date(),
+      };
+      await TokenCollection.insertOne(token);
+      const url = `${process.env.BASE_URL}/users/${newUser.local.username}/verify/${token.token}`;
+      await sendCode(email, url);
+      return res
+        .status(201)
+        .json({
+          message: "An Email has been sent to your account, please verify",
+        });
     } catch (error) {
       console.log(error);
-      return res.status(500).json({ error });
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+
+  static async verifyUser(req, res) {
+    const { username, token } = req.params;
+    const collection = await dbClient.db.collection("Users");
+    const TokenCollection = await dbClient.db.collection("token");
+    try {
+      const user = await collection.findOne({ "local.username": username });
+      const validToken = await TokenCollection.findOne({
+        token: token,
+        username: username,
+      });
+      if (!user) {
+        return res.status(400).json({ error: "Invalid Link" });
+      }
+      if (!validToken) {
+        return res.status(400).json({ error: "Invalid Link" });
+      }
+      await collection.findOneAndUpdate(
+        { "local.username": username, "local.verified": false },
+        { $set: { "local.verified": true } },
+        { returnDocument: "after" }
+      );
+      await TokenCollection.deleteOne({ username: username });
+      res.status(200).json({ message: "Email verification was successfull" });
+    } catch (err) {
+      return res.status(500).json({ error: "Internal Server Error" });
     }
   }
 }
-
 module.exports = userAuthentication;
---
+++ utils/welcomeMessage.js
@@ -0,0 +1,69 @@
+const nodeMailer = require('nodemailer');
+
+async function sendCode(to, link){
+ const transporter = nodeMailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'devowoyemi123@gmail.com',
+            pass: 'imkpvizhfzjpbaum'
+        }
+    });
+
+  const mailOptions ={
+    from: "devowoyemi123@gmail.com",
+    to: to,
+    subject: "Email Verification Link",
+    html: `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body style="background-color: #fff; padding: 0; margin: 0; color: #000;">
+  <table width="100%" cellspacing="0" cellpadding="0" bgcolor="#fff" style="background-color: #fff; padding: 50px 10px; color: #000">
+    <tr>
+        <td style="text-align: center; padding-bottom: 20px;">
+          <a href="#">
+            <img src="https://res.cloudinary.com/dxqgshzri/image/upload/v1700353699/brand_uqk9tw.jpg" width="45" height="40px" alt="Chakra">
+          </a>
+        </td>
+      </tr>
+    <tr>
+      <td align="center" valign="top" style="padding: 0px 30px; font-family: Arial, Helvetica, sans-serif; font-size: 10.5px;">
+        <table width="500" cellspacing="0" cellpadding="0" bgcolor="#fff" style="background-color: #fff; border: 1px solid #fff; border-radius: 5px; padding: 20px;">
+         
+         <tr>
+            <td style="text-align: left;">
+              <h3 style="font-size: 18px; margin-top: 10px; margin-bottom: 10px;"> <span style="color: #7E8FF2;"> Welcome to Chakra, </span> this is your confirmation link</h3>
+              <a href=${link} style="display: inline-block; padding: 10px 20px; background-color: #4CAF50; color: #fff; text-decoration: none; border-radius: 5px; font-size: 16px">Verify</a>
+            </td>
+          </tr>
+        </table>
+        <div style="text-align: left;">
+          <p>This email was sent from an unmonitored mailbox.</p>
+          <a href="#" style="color: #6762ed; text-decoration: none;">Unsubscribe</a> |
+          <a href="#" style="color: #6762ed; text-decoration: none;">Privacy Statement</a>
+          <p>Chakra Supports, One Juliene Way, Redmond, WA 98052 USA</p>
+          <img src="https://res.cloudinary.com/dxqgshzri/image/upload/v1700353699/brand_uqk9tw.jpg" width="45" height="40px" alt="Chakra">
+          <p>Thanks from Chakra Community</p>
+        </div>
+      </td>
+    </tr>
+  </table>
+</body>
+</html>
+`
+  }
+  await transporter.sendMail(mailOptions, (error, info) => {
+    if(error){
+        console.log(error)
+        return error
+    }else{
+        console.log(info.response)
+        return info.response
+    }
+  })
+}
+
+module.exports = sendCode;
